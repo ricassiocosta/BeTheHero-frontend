1a8c39c92a64323da4c2f2851767682f96ee6f72
ricassiocosta <riksiocosta@gmail.com>
2020-03-28 21:35:16 -0300

    create new incident feature fixed

--- src/pages/NewIncident/index.js
+++ src/pages/NewIncident/index.js
@@ -1,14 +1,39 @@
 import React, { useState } from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useHistory } from 'react-router-dom'
 import { FiArrowLeft } from 'react-icons/fi'
 
 import './styles.css'
+import api from '../../services/api'
 import logoImg from '../../assets/logo.svg'
 
 export default function NewIncident () {
   const [title, setTitle] = useState('')
   const [description, setDescription] = useState('')
   const [value, setValue] = useState('')
+
+  const ongId = localStorage.getItem('ongId')
+  const history = useHistory()
+
+  async function handleNewIncident (event) {
+    event.preventDefault()
+
+    const data = {
+      title,
+      description,
+      value
+    }
+
+    try {
+      await api.post('incidents', data, {
+        headers: {
+          Authorization: ongId
+        }
+      })
+      history.push('/profile')
+    } catch (error) {
+      alert('erro ao cadastrar caso, tente novamente.')
+    }
+  }
 
   return (
     <div className="new-incident-container">
@@ -22,7 +47,7 @@ export default function NewIncident () {
             Voltar para home
           </Link>
         </section>
-        <form>
+        <form onSubmit={handleNewIncident}>
           <input
             placeholder="Título do caso"
             value={title}
